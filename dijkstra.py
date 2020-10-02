489aac6058e1d73db393abab4e74207bef5651e5
unknown <harry_jphch@hotmail.es>
2020-10-02 15:41:27 -0500

    Modify dijkstra algorithm

--- dijkstra.py
+++ dijkstra.py
@@ -23,10 +23,10 @@ class Graph():
                 min_index = v 
         return min_index
 
-    def dijkstra(self, src): 
+    def dijkstra(self, initPoint): 
    
         dist = [sys.maxsize] * self.V 
-        dist[src] = 0
+        dist[initPoint] = 0
         shortPathSet = [False] * self.V 
    
         for _ in range(self.V): 
@@ -42,15 +42,15 @@ class Graph():
 
 start_time = time.time()   
 g = Graph(9) 
-g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0], 
-           [4, 0, 8, 0, 0, 0, 0, 11, 0], 
+g.graph = [[0, 4, 0, 3, 0, 0, 4, 8, 0], 
+           [4, 0, 8, 0, 5, 0, 2, 11, 0], 
            [0, 8, 0, 7, 0, 4, 0, 0, 2], 
            [0, 0, 7, 0, 9, 14, 0, 0, 0], 
-           [0, 0, 0, 9, 0, 10, 0, 0, 0], 
+           [0, 1, 0, 9, 0, 10, 0, 0, 0], 
            [0, 0, 4, 14, 10, 0, 2, 0, 0], 
-           [0, 0, 0, 0, 0, 2, 0, 1, 6], 
-           [8, 11, 0, 0, 0, 0, 1, 0, 7], 
-           [0, 0, 2, 0, 0, 0, 6, 7, 0]]; 
+           [0, 2, 0, 0, 0, 2, 0, 1, 6], 
+           [8, 11, 0, 0, 4, 0, 1, 0, 7], 
+           [7, 0, 2, 0, 0, 0, 6, 7, 0]]; 
 
 g.dijkstra(0)
 print("Tiempo de ejecución:  %s segundos" % (time.time() - start_time))
