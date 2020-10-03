7c2859807c011aba900799de76b17e2bbdfe6ccb
Stephano Morales <stephanoml@hotmail.com>
2020-10-03 10:10:24 -0500

    Bot 1 hecho con tablero. Tiene datos de entrada, nro de barreras y nro de jugadores

--- Quoridor.py
+++ Quoridor.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 import random as rd
+from datetime import datetime
 
 
-# In[252]:
+# In[2]:
 
 
 def dibujar(fila, columna, muros_v, muros_h):
@@ -40,7 +41,7 @@ def dibujar(fila, columna, muros_v, muros_h):
     plt.show()
 
 
-# In[73]:
+# In[3]:
 
 
 def Create_muros(n):
@@ -79,7 +80,7 @@ def Create_muros(n):
     return l_h, l_v
 
 
-# In[82]:
+# In[4]:
 
 
 g1 = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -123,7 +124,7 @@ g4 = [[1, 1, 1, 1, 1, 1, 1, 1, 1],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],]
 
 
-# In[317]:
+# In[5]:
 
 
 def camino(arr, root, x, y):
@@ -138,7 +139,7 @@ def camino(arr, root, x, y):
     
     return arr
 
-def Algoritmo1(posx, posy, g):
+def Algoritmo1(posx, posy, g):    
     n = 9
 
     visitado = [[False for i in range(n)] for j in range(n)]
@@ -176,10 +177,7 @@ def Algoritmo1(posx, posy, g):
     def BFS(fila, columna, g):
         aux.append((fila, columna))
         visitado[fila][columna] = True
-        distancia = [[False for i in range(n)] for j in range(n)]
         root = [[None for i in range(n)] for j in range(n)]
-    
-        distancia[fila][columna] = 0
     
         while len(aux) > 0:
             x, y = aux.pop(0)
@@ -191,7 +189,6 @@ def Algoritmo1(posx, posy, g):
                     visitado[nx][ny] = True
                     aux.append((nx, ny))
                     root[nx][ny] = x, y
-                    distancia[nx][ny] = distancia[x][y] + 1
                     if g[nx][ny] == 1:
                         return root, nx, ny
     
@@ -199,10 +196,10 @@ def Algoritmo1(posx, posy, g):
     return raiz, x, y           
 
 
-# In[348]:
+# In[6]:
 
 
-def Start(place, activacion):
+def Start(activacion):
     cont, cont2, cont3, cont4 = 0, 0, 0, 0
     arr = []
     arr2 = []
@@ -211,25 +208,25 @@ def Start(place, activacion):
     arr_x = []
     arr_y = []
     
-    if place[0] == 'top' and activacion[0] == 1:
+    if activacion[0] == 1:
         root1, x1, y1 = Algoritmo1(0, 4, g1)
         arr = camino(arr, root1, x1, y1)
         aux = [x1, y1]
         arr.append(aux)
         
-    if place[1] == 'bottom' and activacion[1] == 1:
+    if activacion[1] == 1:
         root2, x2, y2 = Algoritmo1(8, 4, g4)
         arr2 = camino(arr2, root2, x2, y2)
         aux2 = [x2, y2]
         arr2.append(aux2)
         
-    if place[2] == 'right' and activacion[2] == 1:
+    if activacion[2] == 1:
         root3, x3, y3 = Algoritmo1(4, 8, g3)
         arr3 = camino(arr3, root3, x3, y3)
         aux3 = [x3, y3]
         arr3.append(aux3)   
         
-    if place[3] == 'left' and activacion[3] == 1:
+    if activacion[3] == 1:
         root4, x4, y4 = Algoritmo1(4, 0, g2)
         arr4 = camino(arr4, root4, x4, y4)
         aux4 = [x4, y4]
@@ -246,15 +243,28 @@ def Start(place, activacion):
             arr_y.append(y1)
             cont += 1
         
-        if cont2 > 0:
-            arr_y.append(y2)
-            arr_x.append(x2)
-            
-        dibujar(arr_y, arr_x, lineas_h, lineas_v)    
-      
-        if cont2 > 0:
-            arr_y.pop()
-            arr_x.pop()
+        if len(arr2) > 0:
+            if cont2 > 0:
+                arr_y.append(y2)
+                arr_x.append(x2)
+            if cont3 > 0:
+                arr_y.append(y3)
+                arr_x.append(x3)
+            if cont4 > 0:
+                arr_y.append(y4)
+                arr_x.append(x4)
+
+            dibujar(arr_y, arr_x, lineas_h, lineas_v)    
+
+            if cont2 > 0:
+                arr_y.pop()
+                arr_x.pop()
+            if cont3 > 0:
+                arr_y.pop()
+                arr_x.pop()
+            if cont4 > 0:
+                arr_y.pop()
+                arr_x.pop()
     
         if cont2 < len(arr2):
             x2 = arr2[cont2][0]
@@ -263,7 +273,22 @@ def Start(place, activacion):
             arr_y.append(y2)
             cont2 += 1 
         
-        dibujar(arr_y, arr_x, lineas_h, lineas_v)
+        if len(arr3) > 0:
+            if cont3 > 0:
+                arr_y.append(y3)
+                arr_x.append(x3)
+            if cont4 > 0:
+                arr_y.append(y4)
+                arr_x.append(x4)
+
+            dibujar(arr_y, arr_x, lineas_h, lineas_v)    
+
+            if cont3 > 0:
+                arr_y.pop()
+                arr_x.pop()
+            if cont4 > 0:
+                arr_y.pop()
+                arr_x.pop()
         
         if cont3 < len(arr3):
             x3 = arr3[cont3][0]
@@ -271,6 +296,17 @@ def Start(place, activacion):
             arr_x.append(x3)
             arr_y.append(y3)
             cont3 += 1
+        
+        if len(arr4) > 0:
+            if cont4 > 0:
+                arr_y.append(y4)
+                arr_x.append(x4)
+
+            dibujar(arr_y, arr_x, lineas_h, lineas_v)    
+
+            if cont4 > 0:
+                arr_y.pop()
+                arr_x.pop()
             
         if cont4 < len(arr4):
             x4 = arr4[cont4][0]
@@ -279,7 +315,7 @@ def Start(place, activacion):
             arr_y.append(y4)
             cont4 += 1 
         
-       # dibujar(arr_y, arr_x, lineas_h, lineas_v)
+        dibujar(arr_y, arr_x, lineas_h, lineas_v)
         
         if cont == len(arr) and len(arr) != 0:
             print("Ganador bot top")
@@ -295,19 +331,28 @@ def Start(place, activacion):
             break
 
 
-# In[349]:
+# In[7]:
 
 
-n_barreras = rd.randint(1, 8)
+print("Numero de barreras que quieres del 1 al 8:")
+n_barreras = int(input())
 
 lineas_h, lineas_v = Create_muros(n_barreras)
 dibujar([], [], lineas_h, lineas_v)
+  
+print("Elige cuantos jugadores quieres del 1 al 4:")
+n = int(input())
 
-
-# In[350]:
-
-
-Start(['top', 'bottom', 'right', 'left'], [1, 1, 0, 0]) 
+if n == 1:
+    activate = [1, 0, 0, 0]
+elif n == 2:
+    activate = [1, 1, 0, 0]
+elif n == 3:
+    activate = [1, 1, 1, 0]
+else:
+    activate = [1, 1, 1, 1]
+    
+Start(activate) 
 
 
 # In[ ]:
